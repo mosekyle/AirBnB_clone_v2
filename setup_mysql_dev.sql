a2c3a79674999cc7a738be333964bf6745e346fe
KxyZinC TECH <84194606+kelvzzy@users.noreply.github.com>
2023-03-22 16:38:48 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
--- Script to create a MySQL server with the database hbnb_dev_db.
+- Script to create a MySQL server with the database hbnb_dev_db.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
