cb26fbdfba67e39a75022b28823b44b70784a122
KxyZinC TECH <84194606+kelvzzy@users.noreply.github.com>
2023-03-22 16:39:56 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
-- Script to create a MySQL server with the database hbnb_test_db.
+-- Script to create a MySQL server with the database hbnb_test_db.
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
