68395a02092579dc47c9de7b1500a7b178d4eda5
KxyZinC TECH <84194606+kelvzzy@users.noreply.github.com>
2023-04-06 18:02:29 -0400

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,4 +15,4 @@ echo "<html>
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restar
+sudo service nginx restart
